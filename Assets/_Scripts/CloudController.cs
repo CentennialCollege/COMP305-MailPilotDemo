93630d07b0d574bab8a9b217e46043cc8cf6f5f1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-09-23 11:25:20 -0400

    added Cloud prefab

---
+++ Assets/_Scripts/CloudController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Speed {
+	public float minSpeed, maxSpeed;
+}
+
+[System.Serializable]
+public class Drift {
+	public float minDrift, maxDrift;
+}
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, yMin, yMax;
+}
+
+
+public class CloudController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public Speed speed;
+	public Drift drift;
+	public Boundary boundary;
+
+	// PRIVATE INSTANCE VARIABLES
+	private float _CurrentSpeed;
+	private float _CurrentDrift;
+
+	// Use this for initialization
+	void Start () {
+		this._Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 currentPosition = gameObject.GetComponent<Transform> ().position;
+		currentPosition.x += this._CurrentDrift;
+		currentPosition.y -= this._CurrentSpeed;
+		gameObject.GetComponent<Transform> ().position = currentPosition;
+		
+		// Check bottom boundary
+		if (currentPosition.y <= boundary.yMin) {
+			this._Reset();
+		}
+	}
+
+	// resets the gameObject
+	private void _Reset() {
+		this._CurrentDrift = Random.Range (drift.minDrift, drift.maxDrift);
+		this._CurrentSpeed = Random.Range (speed.minSpeed, speed.maxSpeed);
+		Vector2 resetPosition = new Vector2 (Random.Range(boundary.xMin, boundary.xMax), boundary.yMax);
+		gameObject.GetComponent<Transform> ().position = resetPosition;
+	}
+}
