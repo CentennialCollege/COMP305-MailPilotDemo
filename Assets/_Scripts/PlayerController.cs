2123ce9f5eed612c1b7ce5ce3ca2dacf8fd3f1cd
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-09-23 12:22:12 -0400

    updated Player Controller Script

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -3,36 +3,45 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	// PUBLIC INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++
-	public Vector2 move = new Vector2 (0.0f, 0.0f);
+	public float speed;
+	public Boundary boundary;
 	
 	// PRIVATE INSTANCE VARIABLES
-	private Vector2 newPosition = new Vector2(0.0f, 0.0f);
+	private Vector2 _newPosition = new Vector2(0.0f, 0.0f);
 	
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Player entered the scene");
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		newPosition = gameObject.GetComponent<Transform> ().position; // current position
+		this._CheckInput ();
+	}
+
+	private void _CheckInput() {
+		this._newPosition = gameObject.GetComponent<Transform> ().position; // current position
 		
 		if (Input.GetAxis ("Horizontal") > 0) {
-			newPosition.x += this.move.x; // add move value to current position
-			gameObject.GetComponent<Transform>().position = newPosition;
-		}
-		if (Input.GetAxis ("Horizontal") < 0) {
-			newPosition.x -= this.move.x; // subtract move value to current position
-			gameObject.GetComponent<Transform>().position = newPosition;
+			this._newPosition.x += this.speed; // add move value to current position
 		}
 		
-		if (Input.GetAxis ("Vertical") > 0) {
-			newPosition.y += this.move.y; // add move value to current position
-			gameObject.GetComponent<Transform>().position = newPosition;
+		if (Input.GetAxis ("Horizontal") < 0) {
+			this._newPosition.x -= this.speed; // subtract move value to current position
 		}
-		if (Input.GetAxis ("Vertical") < 0) {
-			newPosition.y -= this.move.y; // subtract move value to current position
-			gameObject.GetComponent<Transform>().position = newPosition;
+
+		this._BoundaryCheck ();
+
+		gameObject.GetComponent<Transform>().position = this._newPosition;
+	}
+
+	private void _BoundaryCheck() {
+		if (this._newPosition.x < this.boundary.xMin) {
+			this._newPosition.x = this.boundary.xMin;
+		}
+
+		if (this._newPosition.x > this.boundary.xMax) {
+			this._newPosition.x = this.boundary.xMax;
 		}
 	}
 }
