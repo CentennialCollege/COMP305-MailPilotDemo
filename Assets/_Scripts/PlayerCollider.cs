32be69aa81131fdb03318ca7867852a6a670b07a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-09-30 11:40:51 -0400

    added scoreboard to project

--- Assets/_Scripts/PlayerCollider.cs
+++ Assets/_Scripts/PlayerCollider.cs
@@ -1,17 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerCollider : MonoBehaviour {
-	
+
+	//PUBLIC INSTANCE VARIABLES
+	public Text scoreLabel;
+	public Text livesLabel;
+	public Text gameOverLabel;
+	public Text finalScoreLabel;
+	public int  scoreValue = 0;
+	public int  livesValue = 5;
+
 	// PRIVATE INSTANCE VARIABLES
 	private AudioSource[] _audioSources; // an array of AudioSources
 	private AudioSource _cloudAudioSource, _islandAudioSource;
+
 
 	// Use this for initialization
 	void Start () {
 		this._audioSources = this.GetComponents<AudioSource> ();
 		this._cloudAudioSource = this._audioSources [1];
 		this._islandAudioSource = this._audioSources [2];
+
+		this._SetScore ();
+		this.gameOverLabel.enabled = false;
+		this.finalScoreLabel.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -21,11 +35,32 @@ public class PlayerCollider : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D otherGameObject) {
 		if (otherGameObject.tag == "Island") {
-			this._islandAudioSource.Play ();
+			this._islandAudioSource.Play (); // play yay sound
+			this.scoreValue += 100; // add 100 points
 		}
 		if (otherGameObject.tag == "Cloud") {
-			this._cloudAudioSource.Play ();
+			this._cloudAudioSource.Play (); // play thunder sound
+			this.livesValue--; // remove one life
+			if(this.livesValue <= 0) {
+				this._EndGame();
+			}
 		}
+		this._SetScore ();
+	}
+
+	// PRIVATE METHODS
+	private void _SetScore() {
+		this.scoreLabel.text = "Score: " + this.scoreValue;
+		this.livesLabel.text = "Lives: " + this.livesValue;
+	}
+
+	private void _EndGame() {
+		Destroy(gameObject);
+		this.scoreLabel.enabled = false;
+		this.livesLabel.enabled = false;
+		this.gameOverLabel.enabled = true;
+		this.finalScoreLabel.enabled = true;
+		this.finalScoreLabel.text = "Score: " + this.scoreValue;
 	}
 	
 }
